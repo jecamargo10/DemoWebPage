ba092b999c2d3504823a2837a3f685bb18b275bb
jecamargo10 <javierecam22@gmail.com>
2018-01-15 13:41:29 -0500

    Super Cambio para almorzar
    asdasd

--- PichinchaDemo/web/js/scripts/index.js
+++ PichinchaDemo/web/js/scripts/index.js
@@ -1,3 +1,7 @@
+$("document").ready(function() {
+console.log("CARGOOOOOOOO");
+openCity(event, 'London');
+});
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -11,3 +15,102 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+var chart = AmCharts.makeChart( "chartdiv", {
+  "type": "pie",
+  "theme": "light",
+  "titles": [{
+    "text": "Preferencias",
+    "size": 13
+}],
+  "dataProvider": [ {
+    "title": "Automóviles",
+    "value": 2500
+  }, {
+    "title": "Restaurantes",
+    "value": 3000
+  } , {
+    "title": "Turismo",
+    "value": 1512
+  } , {
+    "title": "Fidelización y puntos",
+    "value": 4350
+  } ],
+  "titleField": "title",
+  "valueField": "value",
+  "labelRadius": 5,
+
+  "radius": "42%",
+  "innerRadius": "60%",
+  "labelText": "[[title]]",
+  "export": {
+    "enabled": true
+  }
+} );
+
+var chart = AmCharts.makeChart("morachart", {
+  "type": "serial",
+  "theme": "light",
+  "marginRight": 70,
+  "titles": [{
+    "text": "Vector de Hábito de Pago",
+    "size": 13
+}],
+  "dataProvider": [
+    {
+      "country": "Enero",
+      "visits": "400"
+    },
+    {
+      "country": "Febrero",
+      "visits": "250"
+    },
+    {
+      "country": "Marzo",
+      "visits": "-50"
+    },
+    {
+      "country": "Abril",
+      "visits": "-40"
+    },
+    {
+      "country": "Junio",
+      "visits": "-20"
+    },
+    {
+      "country": "Julio",
+      "visits": "45"
+    },
+    {
+      "country": "Agosto",
+      "visits": "102"
+    }
+  ],
+  "valueAxes": [{
+    "axisAlpha": 0,
+    "position": "left",
+    "title": "Pago de cuota(USD)"
+  }],
+  "startDuration": 1,
+  "graphs": [{
+    "balloonText": "<b>[[category]]: [[value]] USD</b>",
+    "fillColorsField": "color",
+    "fillAlphas": 0.9,
+    "lineAlpha": 0.2,
+    "type": "column",
+    "valueField": "visits"
+  }],
+  "chartCursor": {
+    "categoryBalloonEnabled": false,
+    "cursorAlpha": 0,
+    "zoomable": false
+  },
+  "categoryField": "country",
+  "categoryAxis": {
+    "gridPosition": "start",
+    "labelRotation": 45
+  },
+  "export": {
+    "enabled": true
+  }
+
+});
